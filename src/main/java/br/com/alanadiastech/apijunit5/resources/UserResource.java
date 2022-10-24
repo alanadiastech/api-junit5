94bc1031aa3ebc64e2f8c8bc25888a1efb6ea787
alanadiastech <ads.pict@gmail.com>
2022-10-24 14:21:27 -0300

    endpoint findAll para user

--- src/main/java/br/com/alanadiastech/apijunit5/resources/UserResource.java
+++ src/main/java/br/com/alanadiastech/apijunit5/resources/UserResource.java
@@ -1,5 +1,6 @@
 package br.com.alanadiastech.apijunit5.resources;
 
+import br.com.alanadiastech.apijunit5.domain.User;
 import br.com.alanadiastech.apijunit5.domain.dto.UserDto;
 import br.com.alanadiastech.apijunit5.services.UserService;
 import org.modelmapper.ModelMapper;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -23,5 +27,13 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserDto> findById(@PathVariable Integer id) {
         return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDto.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>> findAll() {
+        return ResponseEntity.ok().body(service.findAll()
+                                .stream()
+                                .map(x -> mapper.map(x, UserDto.class))
+                                .collect(Collectors.toList()));
     }
 }
--- src/main/java/br/com/alanadiastech/apijunit5/services/UserService.java
+++ src/main/java/br/com/alanadiastech/apijunit5/services/UserService.java
@@ -2,7 +2,11 @@ package br.com.alanadiastech.apijunit5.services;
 
 import br.com.alanadiastech.apijunit5.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 }
