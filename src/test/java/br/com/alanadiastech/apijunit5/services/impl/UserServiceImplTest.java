2806a6fa5f14b5a79f3001866e27441b8caef168
alanadiastech <ads.pict@gmail.com>
2022-10-24 17:39:15 -0300

    teste da classe user service, metodo findAll

--- src/test/java/br/com/alanadiastech/apijunit5/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/alanadiastech/apijunit5/services/impl/UserServiceImplTest.java
@@ -4,7 +4,6 @@ import br.com.alanadiastech.apijunit5.domain.User;
 import br.com.alanadiastech.apijunit5.domain.dto.UserDto;
 import br.com.alanadiastech.apijunit5.repositories.UserRepository;
 import br.com.alanadiastech.apijunit5.services.exceptions.ObjectNotFoundException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -13,6 +12,7 @@ import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,6 +26,7 @@ class UserServiceImplTest {
     public static final String NAME = "Nome";
     public static final String EMAIL = "nome@gmail.com";
     public static final String PASSWORD = "123";
+    public static final int INDEX = 0;
 
     @InjectMocks
     private UserServiceImpl service;
@@ -75,7 +76,21 @@ class UserServiceImplTest {
     }
 
     @Test
-    void findAll() {
+    void WhenFindAllThenReturnAnListOfUsers() {
+        when(repository.findAll()).thenReturn(List.of(user));
+
+        List<User> response = service.findAll();
+
+        assertNotNull(response);
+        assertEquals(1, response.size());
+        assertEquals(User.class, response.get(INDEX).getClass());
+
+        assertEquals(ID, response.get(INDEX).getId());
+        assertEquals(NAME, response.get(INDEX).getName());
+        assertEquals(EMAIL, response.get(INDEX).getEmail());
+        assertEquals(PASSWORD, response.get(INDEX).getPassword());
+
+
     }
 
     @Test
