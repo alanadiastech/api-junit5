d723fd2d4bb197cfe2bdc2f0620226dfb4e4b55a
alanadiastech <ads.pict@gmail.com>
2022-10-24 09:14:52 -0300

    iniciando projeto

---
+++ src/main/java/br/vom/alanadiastech/apijunit5/ApiJunit5Application.java
@@ -0,0 +1,13 @@
+package br.vom.alanadiastech.apijunit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJunit5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJunit5Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/vom/alanadiastech/apijunit5/ApiJunit5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.vom.alanadiastech.apijunit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJunit5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
