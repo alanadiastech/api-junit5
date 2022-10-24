4a8c7171e9c77153979bad04eb9502e86feacf02
alanadiastech <ads.pict@gmail.com>
2022-10-24 09:35:03 -0300

    classe user criada

---
+++ src/main/java/br/vom/alanadiastech/apijunit5/domain/User.java
@@ -0,0 +1,29 @@
+package br.vom.alanadiastech.apijunit5.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
